2cd9cbd8e4e95230fb3811108acd85eb3b741c38
Vivs <vivek_knit@yahoo.co.in>
2012-04-10 16:55:39 +0530

    Fixed test case

--- src/test/java/com/impetus/kundera/examples/crossdatastore/useraddress/TwinAssociation.java
+++ src/test/java/com/impetus/kundera/examples/crossdatastore/useraddress/TwinAssociation.java
@@ -84,6 +84,7 @@ public abstract class TwinAssociation extends AssociationBase
         }
         } catch(Exception e)
         {
+            e.printStackTrace();
             log.error(e);
             Assert.fail("Failure caused by:"+ e.getMessage());
         }
